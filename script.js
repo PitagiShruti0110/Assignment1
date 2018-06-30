9dd746961bc64fb20afeea7838185088a7795134
PitagiShruti0110 <40717666+PitagiShruti0110@users.noreply.github.com>
2018-06-30 19:20:59 +0530

    Fibonacci series which will print values <33

--- script.js
+++ script.js
@@ -1,35 +1,41 @@
-var arrayList = [];
+//Fibonacci Series Method 1
 
-function add() {
-   var arrNum = document.getElementById('num');
-   arrayList.push(arrNum.value);
-   arrNum.value = '';
-}
-function show() {
-   var html = '';
-   for (var i=0; i<arrayList.length; i++) {
-      html += arrayList[i]+" " ;
-   }
-   var con = document.getElementById('container');
-   con.innerHTML = html;
-}
-function findTargetNum(Tnum){
+function Fibonacci(num){
 	var i;
-	var x = parseInt(Tnum);
-	var sortedArray = arrayList.sort(function(a, b){return a - b});
-	
-	for(i=0; i<sortedArray.length;i++){
-		if(sortedArray[i] < x){
-			if(sortedArray.length == (i+1)){
-				document.getElementById("result").innerHTML = sortedArray[i];
-				break;
-			}
-		}
-		else{
-			
-			document.getElementById("result").innerHTML = sortedArray[i];
+    var fib = []; // Initialize array!
+	var n = parseInt(num);
+    
+    fib[0] = 0;
+    fib[1] = 1;
+	document.write(fib[0] + "<br/>");
+	document.write(fib[1] + "<br/>");
+    for(i=2; i<=n; i++)
+    {
+        // Next fibonacci number = previous + one before previous
+        fib[i] = fib[i-2] + fib[i-1];
+       
+	   if(fib[i] >= 33){
 			break;
-		}		
-	}
+		}
+		document.write(fib[i] + "<br/>");		
+    }
 }
 
+//Fibonacci Series Method 2
+function on_btn_click() {
+				count = parseInt(document.getElementById("inCount").value);
+				a = 0, b = 1, sum = 0;
+				output = "<b>The first " + count + " elements in the Fibonacci series:</b> ";
+				// Next fibonacci number = previous + one before previous
+				for(i = 0; i < count; i ++) {
+					output += a + " ";
+					sum = a + b;
+					a = b;
+					b = sum;
+					 if(sum > 34){
+						break;
+					}
+				}
+
+				document.getElementById("pSeries").innerHTML = output;
+}
