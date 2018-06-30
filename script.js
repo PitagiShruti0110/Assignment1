ed4ed285e81d7f5e398a1528463a59db292f3fb4
PitagiShruti0110 <40717666+PitagiShruti0110@users.noreply.github.com>
2018-06-30 19:18:04 +0530

    Added File of Max repeated array string

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 var arrayList = [];
 
 function add() {
-   var arrNum = document.getElementById('num');
-   arrayList.push(arrNum.value);
-   arrNum.value = '';
+   var inp = document.getElementById('strValue');
+   arrayList.push(inp.value);
+   inp.value = '';
 }
+
 function show() {
    var html = '';
    for (var i=0; i<arrayList.length; i++) {
-      html += arrayList[i]+" " ;
+      html += arrayList[i] + ' ';
    }
    var con = document.getElementById('container');
    con.innerHTML = html;
 }
-function findTargetNum(Tnum){
-	var i;
-	var x = parseInt(Tnum);
-	var sortedArray = arrayList.sort(function(a, b){return a - b});
-	
-	for(i=0; i<sortedArray.length;i++){
-		if(sortedArray[i] < x){
-			if(sortedArray.length == (i+1)){
-				document.getElementById("result").innerHTML = sortedArray[i];
-				break;
-			}
-		}
-		else{
-			
-			document.getElementById("result").innerHTML = sortedArray[i];
-			break;
-		}		
-	}
-}
 
+function repeatedArray(){
+var maxRepeat = 1;
+var max = 0;
+var item;
+
+for (var i=0; i<arrayList.length; i++)
+	{
+		for (var j=i; j<arrayList.length; j++)
+			{
+                if (arrayList[i] == arrayList[j])
+                 max++;
+                if (maxRepeat<max)
+                {
+                  maxRepeat=max; 
+                  item = arrayList[i];
+                }
+        }
+        max=0;
+	}
+document.getElementById("foundStr").innerHTML = item;
+}
